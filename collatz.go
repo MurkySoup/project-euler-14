18161d8de9ae5396efc1822e6723572c0b6372f1
Rick Pelletier <rpelletier@gannett.com>
2019-06-19 09:42:12 -0400

    Create collatz.go

---
+++ collatz.go
@@ -0,0 +1,59 @@
+package main
+
+// project euler 14
+// see: http://projecteuler.net/index.php?section=problems&id=014
+
+import (
+        "fmt"
+        "runtime"
+        "time"
+)
+
+// accept an int 'n', return length of collatz sequence for that integer
+func collatz(number int) int {
+        count := 1
+
+        for {
+                if number > 1 {
+                        if number & 1 == 0 {
+                                // number = number / 2
+                                number /= 2
+                        } else {
+                                number = (3 * number) + 1
+                        }
+
+                } else {
+                        break // collatz sequence end at '1'
+                }
+                count++
+        }
+
+        return count
+}
+
+func main() {
+        limit := 1000000
+
+        runtime.GOMAXPROCS(4)
+        start := time.Now()
+
+        hi_val := 0
+        hi_seq := 0
+
+        for number := 2; number <= limit; number++ {
+                go func(number int) {
+                        seq_length := collatz(number)
+                        if seq_length > hi_seq {
+                                hi_seq = seq_length
+                                hi_val = number
+                        }
+                }(number)
+        }
+
+        elapsed := time.Since(start)
+
+        fmt.Printf("\nnumber = %d, seq len = %d\n", hi_val, hi_seq)
+        fmt.Printf("\nExecution time: %s\n\n", elapsed)
+}
+
+// end of script
