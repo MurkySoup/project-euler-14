d0c3a3d8bce22997366dc276fd54d34fc82b7491
Rick Pelletier <rpelletier@gannett.com>
2024-12-29 14:29:16 -0600

    Update collatz.go
    Pull out bad code. Put in good code.

--- collatz.go
+++ collatz.go
@@ -1,59 +1,36 @@
 package main
 
-// project euler 14
-// see: https://projecteuler.net/problem=14
-
 import (
         "fmt"
-        "runtime"
-        "time"
 )
 
-// accept an int 'n', return length of collatz sequence for that integer
-func collatz(number int) int {
-        count := 1
+func CollatzSequence(n int) int {
+        result := 0
 
-        for {
-                if number > 1 {
-                        if number & 1 == 0 {
-                                // number = number / 2
-                                number /= 2
-                        } else {
-                                number = (3 * number) + 1
-                        }
-
+        for n != 1 {
+                result ++
+                if n % 2 == 0 {
+                        n /= 2
                 } else {
-                        break // collatz sequence end at '1'
+                        n = (3 * n) + 1
                 }
-                count++
         }
 
-        return count
+        return result
 }
 
 func main() {
-        limit := 1000000
+        number := int(1)
+        max := 1
 
-        runtime.GOMAXPROCS(4)
-        start := time.Now()
-
-        hi_val := 0
-        hi_seq := 0
-
-        for number := 2; number <= limit; number++ {
-                go func(number int) {
-                        seq_length := collatz(number)
-                        if seq_length > hi_seq {
-                                hi_seq = seq_length
-                                hi_val = number
-                        }
-                }(number)
+        for n := int(1); n < 1000000; n++ {
+                length := CollatzSequence(n)
+                if length > max {
+                        number = n
+                        max = length
+                }
         }
 
-        elapsed := time.Since(start)
-
-        fmt.Printf("\nnumber = %d, seq len = %d\n", hi_val, hi_seq)
-        fmt.Printf("\nExecution time: %s\n\n", elapsed)
+        fmt.Printf("\nNumber : %d\n", number)
+        fmt.Printf("Collatz: %d\n\n", max + 1)
 }
-
-// end of script
